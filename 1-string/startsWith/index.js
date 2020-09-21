440a62333fbca30bbf5918986c12ee0edf3360ab
Suerta <suerta__@hotmail.com>
2020-09-21 20:10:27 +0800

    [Zhang Jindong] feat: pass statsWith test

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,3 +1,9 @@
 export default function collectCarNumberCount(collection) {
   // TODO 7: 在这里写实现代码
+  return collection.reduce((count, item) => {
+    if (item.startsWith('粤A')) {
+      return count + 1;
+    }
+    return count;
+  }, 0);
 }
