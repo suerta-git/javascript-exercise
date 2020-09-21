9adb491a8476972894e8663b198720676185a724
Suerta <suerta__@hotmail.com>
2020-09-21 20:31:05 +0800

    [Zhang Jindong] feat: should use assign

--- 3-object/assign/index.js
+++ 3-object/assign/index.js
@@ -1,3 +1,8 @@
 export default function addSerialNumber(source) {
   // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign
+  const result = {};
+  Object.assign(result, source);
+  result.serialNumber = '12345';
+  result.properties.status = 'processed';
+  return result;
 }
