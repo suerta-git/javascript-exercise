32dd2b1d307ae856901e32ed1f91232282034e8d
Suerta <suerta__@hotmail.com>
2020-09-21 21:23:51 +0800

    [Zhang Jindong] feat: should use extended method

--- 4-class/practice_4/worker.js
+++ 4-class/practice_4/worker.js
@@ -1,1 +1,9 @@
 // TODO 17: 在这里写实现代码
+import Person from './person';
+
+export default class Worker extends Person {
+  // eslint-disable-next-line class-methods-use-this
+  introduce() {
+    return `${this.basic_introduce()} I am a Worker. I have a job.`;
+  }
+}
