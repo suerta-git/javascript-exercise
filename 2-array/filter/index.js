c0fd6f4026c60bd0b8110260943556d6fa4b2e54
Suerta <suerta__@hotmail.com>
2020-09-21 20:26:15 +0800

    [Zhang Jindong] feat: should use filter

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,17 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  return collection.filter(number => number % 3 === 0);
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  const result = [];
+  collection.forEach(number => {
+    if (!result.includes(number)) {
+      result.push(number);
+    }
+  });
+  return result;
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
