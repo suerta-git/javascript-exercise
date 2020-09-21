dc4c70be00897218646fdcbddf01cea469cc3bc8
Suerta <suerta__@hotmail.com>
2020-09-21 21:31:22 +0800

    [Zhang Jindong] feat: should use expression

--- 4-class/practice_5/teacher.js
+++ 4-class/practice_5/teacher.js
@@ -1,1 +1,15 @@
 // TODO 20: 在这里写实现代码
+import Person from './person';
+
+export default class Teacher extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    return `${this.basic_introduce()} I am a Teacher. I teach ${
+      this.klass === undefined ? 'No Class' : `Class ${this.klass}`
+    }.`;
+  }
+}
