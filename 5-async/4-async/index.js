b5f8a40c90ccd04ed9f5d430e80e4e4fce1f7798
Suerta <suerta__@hotmail.com>
2020-09-21 22:15:02 +0800

    [Zhang Jindong] feat: should use async and await

--- 5-async/4-async/index.js
+++ 5-async/4-async/index.js
@@ -1,6 +1,9 @@
 async function fetchData(url) {
   // <-- start
   // TODO 24: 通过await/async实现异步请求
+  const response = await fetch(url);
+  const result = await response.json();
+  document.writeln(result.name);
   // end -->
 }
 
